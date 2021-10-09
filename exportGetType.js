77e1338cddc0fa2baf743475110b092defe9ac6c
8wallDawn <dankthedust@gmail.com>
2021-10-09 19:31:48 +0900

    lodashTest 브랜치 생성

--- exportGetType.js
+++
@@ -1,6 +0,0 @@
-// export를 통해서 특정 함수를 다른 js 내에서도 활용할 수 있도록 하며,
-// import를 받는 js는 제공받는 js문서를 명시한다.
-export default function (data){
-    // return Object.prototype.toString.call(data)
-    return Object.prototype.toString.call(data).slice(8,-1)
-}
